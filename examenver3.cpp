b20bef76f5ea32550f4d156a922e3d7df40a1ef8
PEREZ GARCIA ULISES DAVID <168507255+ULISES-DAVID@users.noreply.github.com>
2024-06-03 10:40:40 -0600

    Update examenver3.cpp

---
+++ examenver3.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+  char password[] = {'2', 'b', 'm', '3', '0', '\0'};
+  char input_password[20];
+  char name[50];
+  char grupo[10];
+  int remaining_attempts = 3;
+  int precio;
+
+  cout << "Hola, ¿cómo te llamas? ";
+  cin.getline(name, 50); 
+
+  cout << "Ingresa tu salon ";
+  cin.getline(grupo, 10);
+
+  int i = 0;
+  while (i < 3) {
+    cout << "Ingresa tu contraseña (te quedan " << 3 - i << " intentos): ";
+    cin.getline(input_password, 20); 
+
+    if (strcmp(password, input_password) == 0) {
+      cout << "¡Contraseña correcta, " << name << "! ¿Cuántas facturas tienes? ";
+      int num_facturas;
+      cin >> num_facturas;
+
+      cout << "Ingrese el precio de la factura: ";
+      cin >> precio;
+
+      if (num_facturas > 0) {
+        cout << "El número ingresado es positivo.\n";
+      } else {
+        cout << "El número no es positivo.\n";
+      }
+
+      cout << "Bienvenido, " << name << endl;
+      break;
+    } else {
+      cout << "Contraseña incorrecta. Prueba de nuevo.\n";
+      i++;
+    }
+  }
+
+  if (i == 3) {
+    cout << "Demasiados intentos. Intenta más tarde.\n";
+  }
+
+  return 0;
+}
