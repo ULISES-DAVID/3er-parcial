2c88af447397a2a950535583ee40c9442297588f
PEREZ GARCIA ULISES DAVID <168507255+ULISES-DAVID@users.noreply.github.com>
2024-06-03 11:03:42 -0600

    Add files via upload

---
+++ vocal.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char l;
+    cout<<"Ingresa la letra: ";
+    cin>>l;
+    if (l=='a'){
+        cout<<"Es una vocal"<<endl;
+    }
+    else if (l=='e'){
+        cout<<"es una vocal"<<endl;
+    }
+    else if (l=='i'){
+        cout<<"es una vocal"<<endl;
+    }
+    else if (l=='o'){
+        cout<<"es una vocal"<<endl;
+    }
+    else if (l=='u'){
+        cout<<"es una vocal"<<endl;
+
+    }
+    else {
+        cout<<"no es una vocal"<<endl;
+    }
+    return 0;
+
+}
