b0d2301bb2e1c35146b08c82cd1c3846f802a0ba
PEREZ GARCIA ULISES DAVID <168507255+ULISES-DAVID@users.noreply.github.com>
2024-06-03 10:01:00 -0600

    Update arreglo14.cpp

--- arreglo14.cpp
+++ arreglo14.cpp
@@ -5,17 +5,16 @@
 using namespace std;
 
 int main() {
-  srand(time(0)); // Inicializa la semilla del generador de números aleatorios
+  srand(time(0)); 
   int matriz[3][3];
   
-  // Llena la matriz con números aleatorios entre 0 y 9
+  
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
-      matriz[i][j] = rand() % 10; // Genera un número aleatorio entre 0 y 9
-    }
+      matriz[i][j] = rand() % 10;       }
   }
 
-  // Imprime la matriz
+
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
       cout << matriz[i][j] << " ";
