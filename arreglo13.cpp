8fad5e925dc9e7374f25aaba06ce7083498fe943
PEREZ GARCIA ULISES DAVID <168507255+ULISES-DAVID@users.noreply.github.com>
2024-06-03 10:03:21 -0600

    Update arreglo13.cpp

--- arreglo13.cpp
+++ arreglo13.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main() {
-  int f, c, acum = 0; //Declaramos las variables
+  int f, c, acum = 0; 
   
   cout << "Digite el numero de filas: ";
   cin >> f;
   cout << "Digite el numero de columnas: ";
   cin >> c;
   
-  int matriz[f][c]; //Creamos la matriz
+  int matriz[f][c]; 
 
   for (int i = 0; i < f; i++) {
     for (int j = 0; j < c; j++) {
@@ -18,8 +18,8 @@ int main() {
     }
   }
   
-  for (int i = 0; i < f; i++) { // Recorremos la matriz
-    acum += matriz[i][0]; //Sumamos los elementos de la columna 1
+  for (int i = 0; i < f; i++) { 
+    acum += matriz[i][0]; 
   }
   
   cout << "Todos los elementos de la columna 1 suman un total de: " << acum << endl;
